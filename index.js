6d769b62533ab985376c7c9486f80b23e70f43d5
firenza <chad.oftedahl@sleepnumber.com>
2021-10-18 09:36:26 -0700

    Add lower case regex option

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const core = require('@actions/core');
 try {
     const secrets = core.getInput('secrets');
     const secretFilterRegex = core.getInput('secret_filter_regex');
+    const lowerCaseRegex = core.getInput('lower_case_regex');
 
     const parsedSecrets = JSON.parse(secrets);
 
@@ -10,8 +11,24 @@ try {
         const secretFilterRegexDefined = secretFilterRegex !== null && secretFilterRegex !== undefined && secretFilterRegex !== ''
 
         if (!secretFilterRegexDefined || secretFilterRegexDefined && attributeName.match(secretFilterRegex)){
-            console.log(`Exporting secret ${attributeName} as environment variable`);
-            core.exportVariable(attributeName, parsedSecrets[attributeName]);
+            
+            variableName = attributeName;
+            variableValue = parsedSecrets[attributeName];
+
+            const lowerCaseRegexDefined = lowerCaseRegex !== null && lowerCaseRegex !== undefined && lowerCaseRegex !== ''
+            
+            if(lowerCaseRegexDefined){
+                variableName = variableName.replace(lowerCaseRegexDefined, function(match){
+                    return match.toLowerCase()
+                })
+
+                console.log(`Exporting secret ${attributeName} as environment variable ${variableName}`);
+            }
+            else{
+                console.log(`Exporting secret ${attributeName} as environment variable`);
+            }
+            
+            core.exportVariable(variableName, variableValue);
         }
     }
 } catch (error) {
